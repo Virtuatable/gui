976d4647ee085fcd85bb53dd5287494f51caf7f1
Vincent Courtois <vincent.courtois@coddity.com>
2022-03-03 09:00:50 +0100

    Add fields to the login form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,31 @@
 <template>
-  <v-container>
-    <h1>Login</h1>
-  </v-container>
+  <v-main class="d-flex align-center full-width">
+    <v-container>
+      <v-row>
+        <v-col cols="6" offset="3">
+          <v-card>
+            <v-card-title>Login</v-card-title>
+            <v-card-content>
+              <v-form class="pa-4">
+                <v-text-field outlined label="Nickname"></v-text-field>
+                <v-text-field outlined label="Password" type="password"></v-text-field>
+              </v-form>
+            </v-card-content>
+            <v-card-actions>
+              <v-btn type="submit" text color="primary">Log in</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
 </template>
+
+<style scoped>
+.login-container {
+  background-color: silver;
+}
+.full-width {
+  height: 100vh;
+}
+</style>
