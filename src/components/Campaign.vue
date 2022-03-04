fb69d2db1f173f7b5bc741a8d4a53fd972097d6f
Vincent Courtois <vincent.courtois@coddity.com>
2022-03-04 13:25:40 +0100

    Enhance the display of a campaign in the list

--- src/components/Campaign.vue
+++ src/components/Campaign.vue
@@ -1,11 +1,22 @@
 <template>
   <v-card class="mx-auto my-auto" max-width="400">
     <v-img class="white--text align-end" v-if="campaign.banner !== ''" :src="campaign.banner" height="200px">
-      <v-card-title>{{ campaign.name }}</v-card-title>
+      <v-card-title class="pb-1">{{ campaign.name }}</v-card-title>
     </v-img>
     <v-card-title v-else>{{ campaign.name }}</v-card-title>
-    <v-card-subtitle>ID : {{ campaign.id }}</v-card-subtitle>
-    <v-card-text>{{ campaign.description }}</v-card-text>
+    <v-card-subtitle class="grey--text pb-1 font-weight-light">ID : {{ campaign.id }}</v-card-subtitle>
+    <v-card-text class="text--primary">{{ campaign.description }}</v-card-text>
+    <v-card-actions class="pt-0">
+      <v-spacer></v-spacer>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-on="on" v-bind="attrs" :to="`/campaigns/${campaign.id}`">
+            <v-icon>mdi-dice-d20-outline</v-icon>
+          </v-btn>
+        </template>
+        <span>Play</span>
+      </v-tooltip>
+    </v-card-actions>
   </v-card>
 </template>
 
