0090cf322be52f476473d7b7fdae6447dc628cda
Vincent Courtois <vincent.courtois@coddity.com>
2022-05-02 15:51:04 +0200

    Remove hash from files names

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   lintOnSave: false,
-
   transpileDependencies: [
     'vuetify',
   ],
+  configureWebpack: {
+    output: {
+      filename: "[name].js",
+      chunkFilename: "[name].chunk.js"
+    },
+  },
+  css: {
+    extract: {
+      filename: "[name].css",
+      chunkFilename: "[name].chunk.css"
+    }
+  }
 };
